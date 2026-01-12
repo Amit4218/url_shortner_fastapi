fac3116ba90ab8134467684732537e83ce19a844
Amit4218 <amitbhagat621@gmail.com>
2026-01-13 02:55:09 +0530

    feat: added a templet for the routes to be created and cleanup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
-from config import Base, engine
+from config import engine
+from schemas import ResponseSchema
+from utils import Base
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
@@ -8,4 +10,24 @@ Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root() -> dict:
-    return {"status": "200", "message": "Api is healthy"}
+    return {"status": 200, "message": "Server is running..."}
+
+
+@app.post("/login")
+async def login():
+    pass
+
+
+@app.post("/register")
+async def register(user):
+    pass
+
+
+@app.post("/short_url")
+async def short_url(data):
+    pass
+
+
+@app.get("/{short_url}")
+async def get_original_url(short_url) -> ResponseSchema:
+    return ResponseSchema(status=200, message="success")
