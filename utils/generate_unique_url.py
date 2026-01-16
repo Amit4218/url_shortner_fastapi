a9bf8e7ee6ac9a8dd86bf7e41e7cc8f65be6e68c
Amit4218 <amitbhagat621@gmail.com>
2026-01-16 19:23:45 +0530

    update: now the functions checks both the urls & unregistered_urls tables to avoid any dublicates

--- utils/generate_unique_url.py
+++ utils/generate_unique_url.py
@@ -3,18 +3,24 @@ from string import ascii_letters, digits
 
 from sqlalchemy.orm import Session
 
-from models import Urls
+from models import NotRegisteredUrls, Urls
 
 
 def generate_unique_url(length: int = 6, db: Session | None = None) -> str:
-    """Generates a unique list of 7 characters by default, for unique url"""
+    """Generates a unique list of 6 characters by default, for unique url"""
     if db is None:
         raise ValueError("Database session cannot be None")
 
     while True:
         unique_url: str = "".join(random.choices(ascii_letters + digits, k=length))
 
-        exits = db.query(Urls).filter(Urls.short_url == unique_url).first()
+        registered_url = db.query(Urls).filter(Urls.short_url == unique_url).first()
 
-        if not exits:
+        temp_urls = (
+            db.query(NotRegisteredUrls)
+            .filter(NotRegisteredUrls.short_url == unique_url)
+            .first()
+        )
+
+        if not registered_url and not temp_urls:
             return unique_url
